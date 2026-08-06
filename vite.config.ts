9710042be0477ee5a6e21fa6c80f4dcc41cee1f3
Rushow <gc11119242530@gmail.com>
2026-08-06 08:41:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  base: '/Knowledge Ball/',
+  base: '/Knowledge-Ball/',
   build: {
     outDir: 'dist',
     target: 'es2020'
